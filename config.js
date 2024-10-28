1a30f05213fd0977dcaf402cd00152846350a519
Faithoghene <faithoghene17@gmail.com>
2024-10-28 18:01:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nTIXhSaD#a2NyKM1re6WjiDsRCy84svjGZMnCDRGiOmdVMt_rTI8",
+SESSION_ID: process.env.SESSION_ID || "bJN0RRZA#Cr8CzJPwnFEZElUrtqQFM8OM9vQ6CCN2VXV8_4EB-9E",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
